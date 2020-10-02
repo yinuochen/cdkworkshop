324a9e979f87eb272f085e7b5f1f184ea5ae3b4e
yinuochen <yinuo.j.chen@gmail.com>
2020-10-02 14:57:10 -0700

    added lambda

--- cdkworkshop/cdkworkshop_stack.py
+++ cdkworkshop/cdkworkshop_stack.py
@@ -1,9 +1,6 @@
 from aws_cdk import (
-    aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
-    core
+    core,
+    aws_lambda as _lambda
 )
 
 
@@ -12,13 +9,10 @@ class CdkworkshopStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        queue = sqs.Queue(
-            self, "CdkworkshopQueue",
-            visibility_timeout=core.Duration.seconds(300),
+        # Defines an AWS Lambda resource
+        my_lambda = _lambda.Function(
+            self, 'HelloHandler',
+            runtime=_lambda.Runtime.PYTHON_3_7,
+            code=_lambda.Code.asset('lambda'),
+            handler='hello.handler',
         )
-
-        topic = sns.Topic(
-            self, "CdkworkshopTopic"
-        )
-
-        topic.add_subscription(subs.SqsSubscription(queue))
